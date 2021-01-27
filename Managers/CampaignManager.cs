4c85fece56a5713dc2b59bb21e393ced0f9399b8
CerenBdk <cerenbdk@gmail.com>
2021-01-27 21:20:06 +0000

    Add files via upload

---
+++ Managers/CampaignManager.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using GameSale.Entities;
+using GameSale.IManagers;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameSale.IManagers
+{
+    public class CampaignManager:ICampaignManager
+    {
+        public static List<Campaign> campaignList = new List<Campaign>();
+        
+        public void Add(Campaign campaign)
+        {
+            if (campaignList.Any(x => x.Title == campaign.Title & x.StartedAt == campaign.StartedAt ))
+            {
+                Console.WriteLine("{0} has already been added.", campaign.Title);
+            }
+            else
+            {
+                campaign.ID = campaignList.Count + 1;
+                campaignList.Add(campaign);
+                Console.WriteLine("{0} has been added.", campaign.Title);
+            } 
+        }
+
+        public void Update(Campaign campaign, Campaign campaign1)
+        {
+            if (campaignList.Any(x => x.ID == campaign.ID))
+            {
+                campaign.Title = campaign1.Title;
+                campaign.StartedAt = campaign1.StartedAt;
+                campaign.CompletedAt = campaign1.CompletedAt;
+                campaign.DiscountRate = campaign1.DiscountRate;
+                Console.WriteLine("{0} has been updated.\n", campaign.Title);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not found.", campaign.Title);
+            }
+        }
+
+        public void Delete(int ID)
+        {
+            if (ID != 0)
+            {
+                if (campaignList.Any(x => x.ID == ID))
+                {
+                    var tempCampaign = campaignList.FirstOrDefault(x => x.ID == ID);
+                    campaignList.Remove(tempCampaign);
+                    Console.WriteLine("{0} has been deleted.", tempCampaign.Title);
+                }
+                else
+                {
+                    Console.WriteLine("{0} is not found.", ID);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Undefined ID number");
+            }
+        }
+
+        public void WriteAll()
+        {
+            if (campaignList.Count > 0)
+            {
+                int count = 1;
+                Console.WriteLine("---------- Campaign List ----------");
+                foreach (var campaign in campaignList)
+                {
+                    Console.WriteLine("{0})ID: {1}\n  Title: {2}\n  Started At: {3}\n  Remain Days: {4}\n  Discount Rate: {5}\n", count, campaign.ID, campaign.Title, campaign.StartedAt, (campaign.CompletedAt - DateTime.Now).Days, campaign.DiscountRate);
+                    count++;
+                }
+            }
+            else
+            {
+                Console.WriteLine("No campaign has been added yet.");
+            }
+        }
+
+        public Campaign FindByID(int ID)
+        {
+            Campaign tempCampaign = new Campaign();
+
+            if (campaignList.Any(x => x.ID == ID))
+            {
+                tempCampaign = campaignList.FirstOrDefault(x => x.ID == ID);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not found.", ID);
+            }
+            
+            return tempCampaign;
+        }
+
+        public List<Campaign> GetList()
+        {
+            return campaignList;
+        }
+    }
+}
---
+++ Managers/UserManager.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using GameSale.Entities;
+using GameSale.IManagers;
+using GameSale.Service;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameSale.IManagers
+{
+    public class UserManager:IUserManager
+    {
+        public static List<User> userList = new List<User>();
+        VerificationService _verificationService;
+
+        public UserManager()
+        {
+            _verificationService = new VerificationService();
+        }
+
+        public void Add(User user)
+        {
+            if(_verificationService.GetVerifiedUserList().Any(x => x.IdentityNumber == user.IdentityNumber))
+            {
+                Console.WriteLine("{0} is already registered.\n", user.IdentityNumber);
+            }
+            else
+            {
+                user.ID = userList.Count + 1;
+                userList.Add(user);
+                _verificationService.Add(user);  
+            }  
+        }
+
+        public void Update(User user, User user1)
+        {
+            if (userList.Any(x => x.ID == user.ID))
+            {
+                user.FirstName = user1.FirstName;
+                user.LastName = user1.LastName;
+                user.Email = user1.Email;
+                user.YearOfBirth = user1.YearOfBirth;
+                Console.WriteLine("{0} {1} has been updated.\n", user.FirstName, user.LastName);
+            }
+            else
+            {
+                Console.WriteLine("{0} {1} is not registered.\n", user.FirstName, user.LastName);
+            }
+        }
+
+        public void Delete(int ID)
+        {
+            if (ID != 0)
+            {
+                if (userList.Any(x => x.ID == ID))
+                {
+                    var tempUser= userList.FirstOrDefault(x => x.ID == ID);
+                    userList.Remove(tempUser);
+                    Console.WriteLine("{0} {1} has been deleted.\n", tempUser.FirstName, tempUser.LastName);
+                }
+                else
+                {
+                    Console.WriteLine("{0} is not found.\n", ID);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Undefined ID number.\n");
+            }
+        }
+
+        public User FindByID(int ID)
+        {
+            User tempUser = new User();
+
+            if (userList.Any(x => x.ID == ID))
+            {
+                tempUser = userList.FirstOrDefault(x => x.ID == ID);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not found.\n", ID);
+            }
+
+            return tempUser;
+        }
+
+        public void WriteAll()
+        {
+            if (userList.Count > 0)
+            {
+                int count = 1;
+                Console.WriteLine("---------- User List ----------");
+                foreach (var user in userList)
+                {
+                    Console.WriteLine("     {0})Identity Number: {1}\n       First Name: {2}\n       Last Name: {3}\n       Birth Date: {4}\n", count, user.IdentityNumber, user.FirstName, user.LastName, user.YearOfBirth);
+                    count++;
+                }
+            }
+            else
+            {
+                Console.WriteLine("No user has been added yet.\n");
+            }
+        }
+
+        public List<User> GetList()
+        {
+            return userList;
+        }
+    }
+}
